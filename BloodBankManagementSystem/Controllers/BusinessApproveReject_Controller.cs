c4a40e1e355a361ec00054931e924cc37859e349
swagat2000 <swagatmishra2000@gmail.com>
2023-07-04 17:17:57 +0530

    final update

--- BloodBankManagementSystem/Controllers/BusinessApproveReject_Controller.cs
+++ BloodBankManagementSystem/Controllers/BusinessApproveReject_Controller.cs
@@ -1,195 +1,149 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using BloodBankManagementSystem.Models;
+using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using Dapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Authorization;
 
 namespace BloodBankManagementSystem.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-
-    public class BusinessApproveReject_Controller : ControllerBase
-    {
-
-        private readonly string connectionString;
-
-        public BusinessApproveReject_Controller(IConfiguration configuration)
-        {
-
-            connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        }
-
-
-        [HttpGet("GetRequestorDetails{requestorId}")]
-
-        public IActionResult GetRequestorDetails(string requestorId)
-        {
-
-            try
-
-            {
-
-                using (var connection = new SqlConnection(connectionString))
-
-                {
-
-                    var query = "SELECT * FROM requestor WHERE RequestorId = @RequestorId";
-
-                    var requestor_ = connection.Query(query, new { RequestorId = requestorId });
-
-                    if (requestor_ == null)
-
-                    {
-
-                        return NotFound("Requestor details not found");
-
-                    }
-
-                    return Ok(requestor_);
-
-                }
-
-            }
-
-            catch (Exception ex)
-
-            {
-
-                return BadRequest("Error :" + ex.Message);
-
-            }
-
-        }
-
-
-
-        [HttpPut("SaveRequestorStatusInfoToDB{requestorId}")]
-
-        public IActionResult SaveRequestorStatusInfoToDB(string requestorId, RequestStatus status)
-
-        {
-
-            try
-
-            {
-
-                using (var connection = new SqlConnection(connectionString))
-
-                {
-
-                    var query = "UPDATE requeststatus SET PatientId = @PatientId , Time_Of_The_Day= @Time_Of_The_Day, Blood_Glucose_Level=@Blood_Glucose_Level , Notes= @Notes WHERE RequestorId = @RequestorId ";
-
-                    status.RequestorId = requestorId;
-
-                    connection.Execute(query, status);
-
-                }
-
-                return Ok();
-
-            }
-
-            catch (Exception ex)
-
-            {
-
-                return BadRequest("Error :" + ex.Message);
-
-            }
-
-        }
-
-
-
-        [HttpGet("GetDonorDetails{donorId}")]
-
-        public IActionResult GetDonorDetails(string donorId)
-
-        {
-
-            try
-
-            {
-
-                using (var connection = new SqlConnection(connectionString))
-
-                {
-
-                    var query = "SELECT * FROM donor WHERE DonorId = @DonorId";
-
-                    var donor_ = connection.Query(query, new { DonorId = donorId });
-
-                    if (donor_ == null)
-
-                    {
-
-                        return NotFound("Requestor details not found");
-
-                    }
-
-                    return Ok(donor_);
-
-                }
-
-            }
-
-            catch (Exception ex)
-
-            {
-
-                return BadRequest("Error :" + ex.Message);
-
-            }
-
-        }
-
-
-
-        [HttpPut("SaveDonorStatusInfoToDB{donorId}")]
-
-        public IActionResult SaveDonorStatusInfoToDB(string donorId, DonateBlood donateBlood)
-
-        {
-
-            try
-
-            {
-
-                using (var connection = new SqlConnection(connectionString))
-
-                {
-
-                    var query = "UPDATE donateblood SET status=@status WHERE DonorId=@DonorId ";
-
-                    donateBlood.DonorId = donorId;
-
-                    connection.Execute(query, donateBlood);
-
-                }
-
-                return Ok();
-
-            }
-
-            catch (Exception ex)
-
-            {
-
-                return BadRequest("Error :" + ex.Message);
-
-            }
-
-        }
-
-
-
-
-
-    }
-
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BusinessApproveReject_Controller : ControllerBase
+	{
+		private readonly string connectionString; 
+		public BusinessApproveReject_Controller(IConfiguration configuration)
+		{
+			connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+		
+		[HttpGet("GetRequestorDetails/{requestorId}")]
+		public IActionResult GetRequestorDetails(string requestorId)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					var query = "SELECT * FROM requestor WHERE RequestorId = @RequestorId";
+					var command = new SqlCommand(query, connection);
+					command.Parameters.AddWithValue("@RequestorId",requestorId);
+
+					var reader=command.ExecuteReader();
+					if(reader.Read())
+					{
+						var requestor = new
+						{
+							RequestorId = reader["RequestorId"].ToString(),
+						};
+						reader.Close();
+						return Ok(requestor);
+					}
+					reader.Close ();
+					return NotFound("Requestor details not found");
+				}
+			}
+			catch(Exception ex)
+			{
+				return BadRequest("Error :" + ex.Message);
+			}
+		}
+
+		[HttpPut("SaveRequestorStatusInfoToDB/{requestorId}")]
+		public IActionResult SaveRequestorStatusInfoToDB(string requestorId, RequestStatus status)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(connectionString))
+				{
+					connection.Open(); 
+					var query = "UPDATE requeststatus SET PatientId = @PatientId , Time_Of_The_Day= @Time_Of_The_Day, Blood_Glucose_Level=@Blood_Glucose_Level , Notes= @Notes WHERE RequestorId = @RequestorId ";
+					var command = new SqlCommand(query, connection);
+					command.Parameters.AddWithValue("@PatientId", status.PatientId);
+					command.Parameters.AddWithValue("@Time_Of_The_Day", status.Time_Of_The_Day);
+					command.Parameters.AddWithValue("@Blood_Glucose_Level", status.Blood_Glucose_Level);
+					command.Parameters.AddWithValue("@Notes", status.Notes);
+					command.Parameters.AddWithValue("@RequestorId", requestorId);
+
+					var affectedRows = command.ExecuteNonQuery();
+					if (affectedRows > 0)
+					{
+						return Ok();
+					}
+
+					return NotFound("Requestor not found or status not updated.");
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("Error :" + ex.Message);
+			}
+		}
+
+		[HttpGet("GetDonorDetails/{donorId}")]
+		public IActionResult GetDonorDetails(string donorId)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(connectionString))
+				{
+					connection.Open(); 
+					var query = "SELECT * FROM donor WHERE DonorId = @DonorId";
+					var command = new SqlCommand(query, connection);
+					command.Parameters.AddWithValue("@DonorId", donorId);
+
+					var reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						var donor = new
+						{
+							Donorld = reader["DonorId"].ToString(),
+						};
+						reader.Close();
+						return Ok(donor);
+					}
+
+					reader.Close();
+					return NotFound("Donor details not found");
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("Error :" + ex.Message);
+			}
+		}
+
+		[HttpPut("SaveDonorStatusInfoToDB/{donorId}")]
+		public IActionResult SaveDonorStatusInfoToDB(string donorId, DonateBlood donateBlood)
+		{
+			try
+			{
+				using (var connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					var query = "UPDATE donateblood SET Status = @Status WHERE DonorId = @DonorId";
+					var command = new SqlCommand(query, connection);
+					command.Parameters.AddWithValue("@Status", donateBlood.Status);
+					command.Parameters.AddWithValue("@DonorId", donorId);
+
+					var affectedRows = command.ExecuteNonQuery();
+					if (affectedRows > 0)
+					{
+						return Ok();
+					}
+
+					return NotFound("Donor not found or status not updated.");
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("Error :" + ex.Message);
+			}
+		}
+
+
+	}
 }
+
--- BloodBankManagementSystem/Controllers/BusinessDonateBloodAndCheck.cs
+++ BloodBankManagementSystem/Controllers/BusinessDonateBloodAndCheck.cs
@@ -2,12 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Dapper;
-using System.Globalization;
+using System.Data;
 using System.Data.SqlClient;
 using BloodBankManagementSystem.Models;
+
 
 namespace BloodBankManagementSystem.Controllers
 {
@@ -15,19 +13,141 @@ namespace BloodBankManagementSystem.Controllers
     [ApiController]
     public class BuisnessDonateBloodAndCheck : ControllerBase
     {
-
-        private readonly string connectionString;
-
-        public BuisnessDonateBloodAndCheck(IConfiguration configuration)
-        {
-            connectionString = configuration.GetConnectionString("DefaultConnection");
-        }
+        private readonly string connectionString = "Data Source=LTIN167901;Initial Catalog=DD_design;Integrated Security=True";
 
 
         [HttpPost]
         public IActionResult SaveDonateBloodRequestToDB(DonateBlood donate)
         {
-            
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string insertQuery = @"INSERT INTO donateblood (DonorId,FullName,DOB,Gender,Blood_Group,City,
+                                            Weight,Date_Of_Last_Donation,How_Many_times,Phone_Number,EmailId,Status)
+                                            VALUES (@Donor_Id,@FullName,@DOB,@Gender,@Blood_Group,@City,@Weight,
+                                            @Date_Of_Last_Donation,@How_Many_Times,@Phone_Number,@EmailId,@Status);";
+
+                    using (var command = new SqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@Donor_Id",donate.DonorId);
+                        command.Parameters.AddWithValue("@FullName",donate.FullName);
+                        command.Parameters.AddWithValue("@DOB",donate.DOB);
+                        command.Parameters.AddWithValue("@Gender",donate.Gender);
+                        command.Parameters.AddWithValue("@Blood_Group", donate.Blood_Group);
+                        command.Parameters.AddWithValue("@City", donate.City);
+                        command.Parameters.AddWithValue("@Weight", donate.Weight);
+                        command.Parameters.AddWithValue("@Date_Of_Last_Donation",donate.Date_Of_Last_Donation);
+                        command.Parameters.AddWithValue("How_Many_Times", donate.How_Many_Times);
+                        command.Parameters.AddWithValue("@Phone_Number", donate.Phone_Number);
+                        command.Parameters.AddWithValue("@EmailId", donate.EmailId);
+                        command.Parameters.AddWithValue("@Status",donate.Status);
+
+                        command.ExecuteNonQuery();
+
+                    }
+
+                }
+
+                return Ok("Donate details saved successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{donorId}")]
+        public IActionResult GetDonorStatus(string donorId)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var query = "SELECT * FROM donor WHERE DonorId = @DonorId";
+
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@DonorId", donorId);
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string DonorId = reader["DonorId"].ToString();
+                                string Firstname = reader["FirstName"].ToString();
+                                string LastName = reader["LastName"].ToString();
+                                string Password = reader["Password"].ToString();
+                                string DOB = reader["DOB"].ToString();
+                                string Address = reader["Address"].ToString();
+                                string ContactNo = reader["ContactNo"].ToString();
+                                string EmailId = reader["EmailId"].ToString();
+                                string Gender = reader["Gender"].ToString();
+                                string BloodGroup = reader["BloodGroup"].ToString();
+
+                                var donor = new
+                                {
+                                    DonorId = donorId,
+                                    Firstname = Firstname,
+                                    LastName = LastName,
+                                    Password = Password,
+                                    DOB = DOB,
+                                    Address = Address,
+                                    ContactNo = ContactNo,
+                                    EmailId = EmailId,
+                                    Gender = Gender,
+                                    BloodGroup = BloodGroup
+                                };
+                                return Ok(donor);
+
+                            }
+                        }
+
+                    }
+                        return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error Occurred while retrieving the requestor details : " + ex.Message);
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public BuisnessDonateBloodAndCheck(IConfiguration configuration)
+        private readonly string connectionString = "Data Source=LTIN167901;Initial Catalog=DD_design;Integrated Security=True";
+{
+    connectionString = configuration.GetConnectionString("DefaultConnection");
+
+}
+
+[HttpPost]
+public IActionResult SaveDonateBloodRequestToDB(DonateBlood donate)
+{
+            /* try
+              {
             using (var connection = new SqlConnection(connectionString))
             {
                 string insertQuery = @"INSERT INTO donateblood (DonorId,FullName,DOB,Gender,Blood_Group,City,
@@ -38,11 +158,11 @@ namespace BloodBankManagementSystem.Controllers
                 }
                 return Ok("Donor details saved successfully.");
             
-            /*catch (Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest("Error Occurred"+ex.Message);
             }
-            */
+            
         }
 
         [HttpGet("{donorId}")]
@@ -66,5 +186,4 @@ namespace BloodBankManagementSystem.Controllers
                 return BadRequest("Error Occurred while retrieving the requestor details : " + ex.Message);
             }
         }
-    }
-}
+         */
